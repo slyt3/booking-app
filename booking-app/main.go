02d936af77c17aacaf5ea4a4248e6b22dae99552
Mantas <mantas.sumin@gmail.com>
2022-01-13 23:29:43 +0200

    First commit

---
+++ booking-app/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	conferenceName := "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets uint = 50
+	var bookings []string
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	// asks user for first name
+
+	fmt.Print("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	// asks user for they last name
+
+	fmt.Print("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	// asks user for they email
+
+	fmt.Print("Enter your email address: ")
+	fmt.Scan(&email)
+
+	//asks user how much tickets do they want
+
+	fmt.Print("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all our bookings: %v\n", bookings)
+
+}
